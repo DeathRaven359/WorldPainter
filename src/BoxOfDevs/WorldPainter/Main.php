7f120b1317fa9368662eb7b060b7e317b343623b
Ad5001 <ad5001yt@gmail.com>
2016-06-23 15:50:43 +0300

    Adding the both mode that replace and place !
    And replace and both mods should work now ! Just need to finish a
    function for the place.

--- src/BoxOfDevs/WorldPainter/Main.php
+++ src/BoxOfDevs/WorldPainter/Main.php
@@ -4,9 +4,11 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
+use pocketmine\block\Block;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\plugin\PluginBase;
 use pocketmine\plugin\Plugin;
+use pocketmine\math\Vector3;
 use pocketmine\Server;
 use pocketmine\scheduler\PluginTask; 
 use pocketmine\Player;
@@ -15,6 +17,7 @@ use pocketmine\Player;
  define("Prefix", C::YELLOW . "[" . C::GOLD . "WorldPainter" . C::YELLOW . "] ", true);
  define("M_PLACE", "PlayerModePlace");
  define("M_REPLACE", "PlayerModeReplace");
+ define("M_BOTH", "PlayerModeBoth");
  define("CG", C::GREEN); // Green color shortcut
  define("CR", C::RED); // Red color shortcut
  define("CY", C::YELLOW); // Yellow color shortcut
@@ -32,6 +35,57 @@ public function onEnable(){
     $this->block = [];
     $this->getServer()->getScheduler()->scheduleRepeatingTask(new PaintTask($this));
 // $this->getServer()->getPluginManager()->registerEvents($this, $this);
+ }
+ 
+ 
+ 
+ public function lastemptyblock(Player $player) {
+     // TO DO
+ }
+ 
+ 
+ 
+ 
+ 
+ public function getBlock(string $cblock) { // Format example: "1:0%90,6:0%10" 
+ 
+     if(strpos($cblock, ",")) { // If there is a proportion.
+         $cblocks = explode(",", $cblock);
+         $blocks = [];
+         $rand = rand(0, 100);
+         $base = 0; // To be sure that there is one possibility
+         foreach($cblocks as $cblock2) {
+             
+             if(strpos($cblock2, "%")) {
+                 $pblock = explode("%", $cblock2);
+                 if($base + $pblock[1] > $rand) {
+                     if(strpos($pblock[0], ":")) {
+                         $ids = explode(":", $pblock[0]);
+                         $id = $ids[0];
+                         $md = $ids[1];
+                     } else {
+                         $id = $pblock[0];
+                         $md = 0;
+                     }
+                     return new Block($id, $md);
+                 } else {
+                     $base = $base + $pblock[1];
+                 }
+             }
+             
+         }
+         
+     } else { // If there is only one block
+         if(strpos($pblock[0], ":")) {
+           $ids = explode(":", $pblock[0]);
+           $id = $ids[0];
+           $md = $ids[1];
+       } else {
+           $id = $pblock[0];
+           $md = 0;
+       }
+       return new Block($id, $md);
+     }
  }
  
  
@@ -95,6 +149,10 @@ public function onEnable(){
                  break;
                  case "place": // Entering in replace mode
                  $this->modes[$sender->getName()] = M_REPLACE;
+                 $sender->sendMessage(PREFIX . CG . "You have succefully entered in Replace mode.");
+                 break;
+                 case "both": // Entering in replace AND player mode
+                 $this->modes[$sender->getName()] = M_BOTH;
                  $sender->sendMessage(PREFIX . CG . "You have succefully entered in Replace mode.");
                  break;
                  default: // Not a correct mode
@@ -187,8 +245,62 @@ class PaintTask extends PluginTask {
     
     
     public function place(Player $player) {
-        switch($this->m->mode[$player->getName()])
-        $player->getTargetBlock(20, $transparent);
+        switch($this->m->mode[$player->getName()]) {
+            
+            case M_PLACE:
+            $centerblock = $this->m->lastemptyblock($player); // TO DO
+            if(!isset($this->m->radius[$player->getName()])) {
+                $this->m->radius->[$player->getName()] = 1;
+            }
+            $r = $this->m->radius[$player->getName()];
+            for($x = $centerblock->x - $r / 2; $x <= $centerblock->x + $r / 2; $x++) {
+                for($y = $centerblock->y - $r / 2; $y <= $centerblock->y + $r / 2; $y++) {
+                    for($z = $centerblock->z - $r / 2; $z <= $centerblock->z + $r / 2; $z++) {
+                        if($player->getLevel()->getBlock(new Vector3($x, $y, $z))->getId() == 0) {
+                            $player->getLevel()->setBlock(new Vector3($x, $y, $z), $this->m->getBlock($this->m->block[$player->getName()]));
+                        }
+                    }
+                }
+            }
+            break;
+            
+            case M_BOTH:
+            $centerblock = $player->getTargetBlock(20);
+            if(!isset($this->m->radius[$player->getName()])) {
+                $this->m->radius->[$player->getName()] = 1;
+            }
+            $r = $this->m->radius[$player->getName()];
+            for($x = $centerblock->x - $r / 2; $x <= $centerblock->x + $r / 2; $x++) {
+                for($y = $centerblock->y - $r / 2; $y <= $centerblock->y + $r / 2; $y++) {
+                    for($z = $centerblock->z - $r / 2; $z <= $centerblock->z + $r / 2; $z++) {
+                        $player->getLevel()->setBlock(new Vector3($x, $y, $z), $this->m->getBlock($this->m->block[$player->getName()]));
+                    }
+                }
+            }
+            break;
+            
+            case M_REPLACE:
+            $centerblock = $player->getTargetBlock(20);
+            if(!isset($this->m->radius[$player->getName()])) {
+                $this->m->radius->[$player->getName()] = 1;
+            }
+            $r = $this->m->radius[$player->getName()];
+            for($x = $centerblock->x - $r / 2; $x <= $centerblock->x + $r / 2; $x++) {
+                for($y = $centerblock->y - $r / 2; $y <= $centerblock->y + $r / 2; $y++) {
+                    for($z = $centerblock->z - $r / 2; $z <= $centerblock->z + $r / 2; $z++) {
+                        if($player->getLevel()->getBlock(new Vector3($x, $y, $z))->getId() !== 0) {
+                            $player->getLevel()->setBlock(new Vector3($x, $y, $z), $this->m->getBlock($this->m->block[$player->getName()]));
+                        }
+                    }
+                }
+            }
+            break;
+            
+            default:
+            $this->m->mode[$player->getName()] = M_PLACE;
+            $player->sendMessage(PREFIX . CY . "Switched to Place mode.");
+            break;
+        }
     }
     
     
